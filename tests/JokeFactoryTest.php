1e6fcd788b077a06c49519872b83507c9b942a30
Mansuang Pawong <mansaung@hotmail.com>
2019-02-25 14:25:41 +0700

    Apply fixes from StyleCI (#2)

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -3,21 +3,21 @@
 namespace Mansuang\Jokes\Tests;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use Mansuang\Jokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
-use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\MockHandler;
 
 class JokeFactoryTest extends TestCase
 {
     /** @test */
     public function it_returns_a_random_joke()
-    {        
+    {
         $mock = new MockHandler([
             new Response(200, [], '{ "type": "success", "value": { "id": 440, "joke": "Most tough men eat nails for breakfast. Chuck Norris does all of his grocery shopping at Home Depot.", "categories": [] } }'),
         ]);
-                
+
         $handler = HandlerStack::create($mock);
 
         $client = new Client(['handler' => $handler]);
