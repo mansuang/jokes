8afc8e699494bba7f76ecd569488b78aa9f67a45
Mansuang <mansaung@hotmail.com>
2019-02-25 14:25:22 +0700

    add api call

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -2,34 +2,29 @@
 
 namespace Mansuang\Jokes\Tests;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
 use Mansuang\Jokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\HandlerStack;
 
 class JokeFactoryTest extends TestCase
 {
     /** @test */
     public function it_returns_a_random_joke()
-    {
-        $jokes = new JokeFactory([
-            'This is a joke',
+    {        
+        $mock = new MockHandler([
+            new Response(200, [], '{ "type": "success", "value": { "id": 440, "joke": "Most tough men eat nails for breakfast. Chuck Norris does all of his grocery shopping at Home Depot.", "categories": [] } }'),
         ]);
+                
+        $handler = HandlerStack::create($mock);
+
+        $client = new Client(['handler' => $handler]);
+
+        $jokes = new JokeFactory($client);
         $joke = $jokes->getRandomJoke();
 
-        $this->assertSame('This is a joke', $joke);
-    }
-
-    /** @test */
-    public function it_returns_a_predefined_joke()
-    {
-        $chuckNorrisJokes = [
-            'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
-            'Chuck Norris counted to infinity... Twice.',
-            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
-        ];
-
-        $jokes = new JokeFactory();
-        $joke = $jokes->getRandomJoke();
-
-        $this->assertContains($joke, $chuckNorrisJokes);
+        $this->assertSame('Most tough men eat nails for breakfast. Chuck Norris does all of his grocery shopping at Home Depot.', $joke);
     }
 }
